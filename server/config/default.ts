c56d51d69692c73e7a89273d8443690e2fe95426
youngking44 <110324517+youngking44@users.noreply.github.com>
2024-10-14 03:29:01 -0700

    Update default.ts

--- server/config/default.ts
+++ server/config/default.ts
@@ -6,6 +6,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 const PORT = process.env.PORT || 1337;
 const DB_URI = process.env.DB_URI;
+const CLIENT_URL = process.env.CLIENT_URL
 const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 const JWT_SECRET = process.env.JWT_SECRET;
 const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
@@ -14,6 +15,7 @@ const SALT_WORK_FACTOR = 10;
 export default {
   port: PORT,
   dbUri: DB_URI,
+  clientUrl: CLIENT_URL,
   webhookSecret: WEBHOOK_SECRET,
   accessToken: JWT_SECRET,
   refreshToken: REFRESH_TOKEN,
